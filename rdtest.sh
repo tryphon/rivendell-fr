3b63d268bb7a381dfc34078501b20ce7e7c71024
Alban Peignier <alban.peignier@free.fr>
2009-08-15 09:49:41 +0200

    Add a script to update /usr/share/rivendell qm file

---
+++ rdtest.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+rivendell_sources=$1
+
+export RD_SOURCE=$rivendell_sources 
+export RD_LANGUAGES=`dirname $0` 
+
+$rivendell_sources/helpers/rdtrans.sh write fr
+
+cd $rivendell_sources
+
+cd rdairplay
+lupdate rdairplay.pro
+lrelease rdairplay.pro
+sudo cp rdairplay_fr.qm /usr/share/rivendell/
